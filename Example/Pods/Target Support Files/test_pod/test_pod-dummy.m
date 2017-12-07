6677fe329470c45a747089f6d9bb0ef37f4976db
Colton Nohelty <noheltycolton@gmail.com>
2017-12-07 16:43:15 -0500

    All the things

---
+++ Example/Pods/Target Support Files/test_pod/test_pod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_test_pod : NSObject
+@end
+@implementation PodsDummy_test_pod
+@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,10 +3,8 @@ import XCTest
 import test_pod
 
 class Tests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
@@ -15,15 +13,6 @@ class Tests: XCTestCase {
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        XCTAssert(true, "Yep, should work")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
